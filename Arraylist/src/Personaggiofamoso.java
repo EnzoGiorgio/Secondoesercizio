0efde3597d5412f13eb1c1cf918539304847f491
vol4ainf-06 <vol4ainf-06@vol06.colamonico.itc>
2022-02-01 10:44:19 +0100

    Primo comment

---
+++ Arraylist/src/Personaggiofamoso.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Personaggiofamoso{
+
+	public static void main(String[] args){
+		
+		
+		class Main {
+		  public void main(String[] args) {
+		    ArrayList<String>  personaggi = new ArrayList<String>();
+		    personaggi.add("barella");
+		    personaggi.add("lautaro");
+		    personaggi.add("maradona");
+		    personaggi.add("r9");
+		    
+		    System.out.println(personaggi);
+		    
+		  }
+		}
+	}
+
+}
